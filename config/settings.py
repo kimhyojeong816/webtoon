87b6bdd3d100290796ade311076c642e3c5755c7
hjpt012 <hjpt012@naver.com>
2019-11-15 16:11:49 +0900

    배포설정

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'tdb0a_r!^^96fab-*2!eaq@wndo(9+t@1)@x_p#r#5z*col_lc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
